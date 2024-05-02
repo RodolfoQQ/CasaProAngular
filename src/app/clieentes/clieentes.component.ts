3e09016d574ed92d01a613e1191755b49ca78936
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-05-02 07:42:51 -0500

    se modifico las tablas ahora envia de valor del c hijo al padre

--- src/app/clieentes/clieentes.component.ts
+++ src/app/clieentes/clieentes.component.ts
@@ -1,15 +1,66 @@
 import { Component } from '@angular/core';
 import { FormComponent } from './form/form.component';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ClienteServiceService } from './service/cliente.service.service';
+import { Cliente } from './models/cliente';
+import { ClientePersona } from './models/clientePersona';
 
 @Component({
   selector: 'app-clieentes',
   standalone: true,
-  imports: [FormComponent,NgIf],
+  imports: [FormComponent,NgIf, NgFor],
   templateUrl: './clieentes.component.html',
   styleUrl: './clieentes.component.css'
 })
 export class ClieentesComponent {
+
+  empresas: Cliente[]=[];
+  empresaSelected: Cliente=new Cliente();
+  
+  tablaEmpresa: boolean=false;
+
+  persona: ClientePersona[]=[];
+
+  constructor(private service:ClienteServiceService){}
+    ngOnInit(): void {
+      
+      this.service.findall().subscribe(data=>this.empresas=data)
+
+    }
+
+    onMostrarTabla(mostrar:boolean){
+      this.tablaEmpresa=mostrar;
+    }
+
+    onUpdate(empresasrow:Cliente){
+      this.empresaSelected={...empresasrow }
+
+    }
+
+
+    addClienteEmpresa(data:Cliente):void{
+      if(data.codEmpresa>0){
+          this.empresas=this.empresas.map(empr=>{
+            if(empr.codEmpresa==data.codEmpresa){
+                return{... data}
+            }
+            return empr;
+          })
+      }else{
+
+        this.empresas= [... this.empresas, {... data, codEmpresa:new Date().getTime()}];
+      }
+      this.empresaSelected= new Cliente();
+      
+
+    }
+    onDelete(cod:number):void{
+      this.empresas=this.empresas.filter(data=>data.codEmpresa !=cod)
+    }
+
+      
+
+
+
 
 }
--- src/app/clieentes/form/form.component.ts
+++ src/app/clieentes/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { Cliente } from '../models/cliente';
 import { ClienteServiceService } from '../service/cliente.service.service';
@@ -17,32 +17,46 @@ import { FormsModule } from '@angular/forms';
 })
 export class FormComponent {
 
-   public clienteempresa: Cliente= new Cliente();
-   empresa: Cliente[]=[];
-   persona: ClientePersona[]=[];
-   
+  //  public clienteempresa: Cliente= new Cliente();
+    public clientepersona: ClientePersona= new ClientePersona();
+     @Input() clienteempresa:Cliente={
+      codEmpresa:0,
+      nombre:'',
+      ruc:'',
+      direccion:'',
+      telefono:'',
+      correo:''
+    }
 
-  habilitado:boolean=false;
+    habilitado:boolean=false;
+
+    habilitartabla:boolean=false;
+    @Output() cambiohabilitartabla= new EventEmitter<boolean>();
 
   setHabiliar(): void{
+    //cambia de formulario de cliente
     this.habilitado=this.habilitado? false: true;
-  }
-  constructor(private service: ClienteServiceService, private servicePer: PersonaServiceService,private router :Router ){}
-
-  ngOnInit(): void {
-    this.service.findall().subscribe(
-      data => this.empresa = data
-    )
-   this.servicePer.findallPersonas().subscribe(
-      data=> this.persona = data)
-      console.log(this.empresa)
-  }
-
-  crear():void{
-      this.service.createClienteEmpre(this.clienteempresa)
-      console.log(this.clienteempresa)
+    
+    //cambiade tabla de cliente
+    //esta es otra opcion al operador ternario
+   // this.habilitartabla =! this.habilitartabla
    
+    this.habilitartabla = this.habilitartabla ? false : true;
+    this.cambiohabilitartabla.emit(this.habilitartabla);
+
   }
+
+  @Output() nuevoClienteEmpresa =new EventEmitter();
+
+
+  crearCliempresa():void{
+        this.nuevoClienteEmpresa.emit(this.clienteempresa)
+        console.log( this.clienteempresa)
+
+        
+    }
+
+  
 
 
 
